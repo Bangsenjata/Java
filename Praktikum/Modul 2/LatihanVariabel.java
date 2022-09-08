4699adc832c437296b7b48653ee9d2675fc8b643
Gita Bangun Prakoso <gpratama477@gmail.com>
2022-09-08 13:46:31 +0700

    Add Modul 2

---
+++ Praktikum/Modul 2/LatihanVariabel.java
@@ -0,0 +1,12 @@
+public class LatihanVariabel {
+    public static void main(String args[]){
+        int a = 10;
+        Double b = 3.0;
+        float c = 4.3f;
+        Double.valueOf(a);
+        Double.valueOf(c);
+        b = a + b + c;
+        System.out.println("Nilai B: " +b);
+        
+    }
+}
